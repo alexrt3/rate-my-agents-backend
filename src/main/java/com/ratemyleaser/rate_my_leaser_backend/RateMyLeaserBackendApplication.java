75522946a7b77d6a700f1eafc8f3acca81793a1e
Alex Tsang <alextsang@Alexs-MacBook-Pro.local>
2025-03-25 12:39:33 -0500

    initial commit

---
+++ src/main/java/com/ratemyleaser/rate_my_leaser_backend/RateMyLeaserBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ratemyleaser.rate_my_leaser_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateMyLeaserBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RateMyLeaserBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ratemyleaser/rate_my_leaser_backend/RateMyLeaserBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ratemyleaser.rate_my_leaser_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateMyLeaserBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
